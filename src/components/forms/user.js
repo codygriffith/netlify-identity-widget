766fb55b0cb35c8bbc842f80a1fa4fd6e2519ad5
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-05-12 14:51:11 -0400

    Update user.js

--- src/components/forms/user.js
+++ src/components/forms/user.js
@@ -5,7 +5,7 @@ import Button from "./button";
 export default class UserForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: "", email: "", password: "", targetPlan: "" };
+    this.state = { name: "", email: "", password: "", targetPlan: window.targetPlan || "Freekenstein" };
   }
 
   handleInput = (e) => {
